dde978346c6daf171634a4312d0ca839a6b630f6
GuillermoSalgado1 <g.salgado04@ufromail.cl>
2025-10-18 01:53:56 -0300

    logo changes and navbar buttons

--- src/component/SideBar/SideBar.jsx
+++ src/component/SideBar/SideBar.jsx
@@ -1,7 +1,7 @@
 import {SideBarIcon} from "../SideBarIcon/SideBarIcon.jsx";
 import './SideBar.css'
 import {useState} from "react";
-import {House, Gear, SignOut} from "@phosphor-icons/react";
+import {House, Gear,ProjectorScreenChart,EnvelopeSimple,BellSimple, SignOut} from "@phosphor-icons/react";
 
 function SideBar() {
 
@@ -10,9 +10,9 @@ function SideBar() {
     return (
         <aside className={'sidebar-container'}>
             <div className={'sidebar-header'}>
-            <div className={'sidebar-logo'}>
-                <img src={"src/icons/logo.svg"} alt="Logo" class/>
-            </div>
+                <div className={'sidebar-logo'}>
+                    <img className={'sidebar-logo-img'} src={"src/icons/logo-new.svg"} alt="Logo"/>
+                </div>
             </div>
 
             <nav className={'sidebar-nav'}>
@@ -22,14 +22,31 @@ function SideBar() {
                     onClick={() => setActiveItem('home')}
                 />
                 <SideBarIcon
+                    icon={ProjectorScreenChart}
+                    isActive={activeItem === 'statistics'}
+                    onClick={() => setActiveItem('statistics')}
+                />
+                <SideBarIcon
+                    icon={EnvelopeSimple}
+                    isActive={activeItem === 'send-notification'}
+                    onClick={() => setActiveItem('send-notification')}
+                />
+                <SideBarIcon
+                    icon={BellSimple}
+                    isActive={activeItem === 'notifications'}
+                    onClick={() => setActiveItem('notifications')}
+                />
+                <SideBarIcon
                     icon={Gear}
                     isActive={activeItem === 'settings'}
                     onClick={() => setActiveItem('settings')}
                 />
                 <SideBarIcon
                     icon={SignOut}
-                    isActive={activeItem === 'logout'}
-                    onClick={() => setActiveItem('logout')}
+                    isActive={false}
+                    onClick={() =>{
+                        console.log('Sign out clicked'); //TODO implement sign out
+                    }}
                 />
             </nav>
         </aside>
