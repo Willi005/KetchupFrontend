f7cc3496d8cd53486ce1045f7a28a6d432318073
GuillermoSalgado1 <g.salgado04@ufromail.cl>
2025-11-07 12:15:09 -0300

    implement filter function component to grid food

--- src/component/OrderPanel/OrderPanel.jsx
+++ src/component/OrderPanel/OrderPanel.jsx
@@ -3,7 +3,7 @@ import './OrderPanel.css';
 export function OrderPanel() {
     return (
         <div className="order-panel">
-            <h2 className="order-title">Orders #34562</h2>
+            <h2 className="order-title">Orders #00000</h2>
             <p>Your order its empty.</p>
         </div>
     )
